1b788aceb2922edb8354d5db0f472ac5b94be98f
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-05 02:00:43 +0000

    added changes to index.html & script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function selects a random number depending on the user's choice.
+ */
+function randomNumber() {
+  // random number
+  const randomNumber = Math.floor(Math.random() * 6) + 1
+
+  // input
+  const numberType = document.getElementById('option-positive').checked
+
+  // process
+  if (numberType == true) {
+    // output
+    document.getElementById('result').innerHTML = 'The random number is: ' + randomNumber
+  } else {
+    // output
+    document.getElementById('result').innerHTML = 'The random number is: ' + (-1 * randomNumber)
+  }
+}
